4833ec3e9539a94a4f12104f52fdd91b84b1851f
reload1bronze <nature9th@gmail.com>
2022-01-11 17:28:06 +0900

    #6.1~6.2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((pre) => pre + 1);
+  const onChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
+  console.log("All the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
 
