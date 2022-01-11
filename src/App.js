4af2e659a7275ad1b72cb5b886db5587f34b7bf9
reload1bronze <nature9th@gmail.com>
2022-01-11 17:11:01 +0900

    #6.0 Intro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((pre) => pre + 1);
   return (
     <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+
       <h1 className={styles.title}>Welcome back!!</h1>
       <Button text={"Continue"} />
     </div>
